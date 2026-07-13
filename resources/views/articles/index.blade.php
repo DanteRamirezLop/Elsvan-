416abe5352bf33174f67fbda1aa8bcfc31cb3f58
Dante Ramirez Lopez <danterldk@gmail..com>
2026-07-13 14:24:51 -0500

    correccion varios banner

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -12,7 +12,8 @@
         </div>
     </section>
 
-    <livewire:whatsapp/>
+     <livewire:marquee/>
+
 
     <section class="section">
         <div class="grid gap-6 md:grid-cols-2 lg:grid-cols-3 my-5">
