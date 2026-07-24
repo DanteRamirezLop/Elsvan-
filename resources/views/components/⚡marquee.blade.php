8b73d6ba6cae3d52dfba0bc41439b96a4c2f5733
Dante Ramirez Lopez <danterldk@gmail..com>
2026-07-24 12:31:50 -0500

    etiqueta en numoer

--- resources/views/components/⚡marquee.blade.php
+++ resources/views/components/⚡marquee.blade.php
@@ -18,7 +18,7 @@ new class extends Component
     <div class="contact-content ">
         <strong class="text-brown text-lg">¡CONTÁCTANOS AHORA!</strong>
         @foreach($phones as $phone)
-        <a href="tel:{{ $phone->dial_number }}" class="text-sm lg:text-base xl:text-lg hover:underline">
+        <a href="tel:{{ $phone->dial_number }}" onclick="return gtag_report_conversion('tel:{{ $phone->dial_number }}');" class="text-sm lg:text-base xl:text-lg hover:underline">
             <svg class="w-6 h-6 text-white inline" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.427 14.768 17.2 13.542a1.733 1.733 0 0 0-2.45 0l-.613.613a1.732 1.732 0 0 1-2.45 0l-1.838-1.84a1.735 1.735 0 0 1 0-2.452l.612-.613a1.735 1.735 0 0 0 0-2.452L9.237 5.572a1.6 1.6 0 0 0-2.45 0c-3.223 3.2-1.702 6.896 1.519 10.117 3.22 3.221 6.914 4.745 10.12 1.535a1.601 1.601 0 0 0 0-2.456Z"/>
             </svg>
@@ -27,3 +27,20 @@ new class extends Component
         @endforeach
     </div>
 </section>
+
+<!-- Event snippet for Clic de llamada en botón del Home conversion page
+In your html page, add the snippet and call gtag_report_conversion when someone clicks on the chosen link or button. -->
+<script>
+function gtag_report_conversion(url) {
+  var callback = function () {
+    if (typeof(url) != 'undefined') {
+      window.location = url;
+    }
+  };
+  gtag('event', 'conversion', {
+      'send_to': 'AW-11336011322/J7m2CO3mgtUcELqct50q',
+      'event_callback': callback
+  });
+  return false;
+}
+</script>
